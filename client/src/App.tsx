40366fa76085d3d25234ce06d81ad94b06d12c47
ansmancai <ansman@qq.com>
2026-03-25 11:06:05 +0800

    修复路由，添加所有详情页和编辑页

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,20 @@ import Layout from './components/Layout';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import ProjectsPage from './pages/ProjectsPage';
-import SuppliersPage from './pages/SuppliersPage';
-import PurchasesPage from './pages/PurchasesPage';
-import TransactionsPage from './pages/TransactionsPage';
-import InvoicesPage from './pages/InvoicesPage';
+import ProjectDetailPage from './pages/ProjectDetailPage';
 import ProjectFormPage from './pages/ProjectFormPage';
+import SuppliersPage from './pages/SuppliersPage';
+import SupplierDetailPage from './pages/SupplierDetailPage';
+import SupplierFormPage from './pages/SupplierFormPage';
+import PurchasesPage from './pages/PurchasesPage';
+import PurchaseDetailPage from './pages/PurchaseDetailPage';
+import PurchaseFormPage from './pages/PurchaseFormPage';
+import TransactionsPage from './pages/TransactionsPage';
+import TransactionDetailPage from './pages/TransactionDetailPage';
+import TransactionFormPage from './pages/TransactionFormPage';
+import InvoicesPage from './pages/InvoicesPage';
+import InvoiceDetailPage from './pages/InvoiceDetailPage';
+import InvoiceFormPage from './pages/InvoiceFormPage';
 
 function PrivateRoute({ children }: { children: React.ReactNode }) {
   const { token } = useAuthStore();
@@ -23,13 +32,36 @@ function App() {
       <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
         <Route index element={<Navigate to="/dashboard" replace />} />
         <Route path="dashboard" element={<DashboardPage />} />
+        
+        {/* 项目 */}
         <Route path="projects" element={<ProjectsPage />} />
         <Route path="projects/new" element={<ProjectFormPage />} />
+        <Route path="projects/:id" element={<ProjectDetailPage />} />
         <Route path="projects/:id/edit" element={<ProjectFormPage />} />
+        
+        {/* 供应商 */}
         <Route path="suppliers" element={<SuppliersPage />} />
+        <Route path="suppliers/new" element={<SupplierFormPage />} />
+        <Route path="suppliers/:id" element={<SupplierDetailPage />} />
+        <Route path="suppliers/:id/edit" element={<SupplierFormPage />} />
+        
+        {/* 采购 */}
         <Route path="purchases" element={<PurchasesPage />} />
+        <Route path="purchases/new" element={<PurchaseFormPage />} />
+        <Route path="purchases/:id" element={<PurchaseDetailPage />} />
+        <Route path="purchases/:id/edit" element={<PurchaseFormPage />} />
+        
+        {/* 收付款 */}
         <Route path="transactions" element={<TransactionsPage />} />
+        <Route path="transactions/new" element={<TransactionFormPage />} />
+        <Route path="transactions/:id" element={<TransactionDetailPage />} />
+        <Route path="transactions/:id/edit" element={<TransactionFormPage />} />
+        
+        {/* 发票 */}
         <Route path="invoices" element={<InvoicesPage />} />
+        <Route path="invoices/new" element={<InvoiceFormPage />} />
+        <Route path="invoices/:id" element={<InvoiceDetailPage />} />
+        <Route path="invoices/:id/edit" element={<InvoiceFormPage />} />
       </Route>
     </Routes>
   );
