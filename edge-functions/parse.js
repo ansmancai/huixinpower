fa3af7f02e6f345ea0dac2ba49472a3b42b71648
ansmancai <ansman@qq.com>
2026-03-31 19:03:48 +0800

    测试改名 parse.js

--- edge-functions/parse.js
+++ edge-functions/parse.js
@@ -1,5 +1,5 @@
 export async function onRequestPost({ request }) {
-  return new Response(JSON.stringify({ message: 'test' }), {
+  return new Response(JSON.stringify({ message: 'ok' }), {
     headers: { 'Content-Type': 'application/json' }
   });
 }
